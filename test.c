020c8fbe47611cea15dc288931f5888099111e5d
Adam Brenecki <adam@brenecki.id.au>
2014-11-02 16:32:23 +1030

    feat(server): Respond correctly to DirectoryList

--- test.c
+++ test.c
@@ -6,10 +6,53 @@
 #include <strings.h>
 #include <inttypes.h>
 #include <netinet/in.h>
+#include <sys/types.h>
+#include <dirent.h>
+#include <mntent.h>
 #include "vendor/svasync/svasync.h"
 #include "message.c"
 #include "util.c"
 
+void do_directory_list(srfp_message *request, srfp_message *response){
+	if (request->header.length == 0){
+		// Empty request body means we need to list available volumes
+		response->body = malloc(26*3); // 26 letters = max 26 volumes
+		response->header.length = 0;
+		// this won't segfault as DJGPP ignores these parameters
+		FILE *mntentptr = setmntent(NULL, NULL);
+		struct mntent *fsdetails;
+		while (fsdetails = getmntent(mntentptr)){
+			memcpy(response->body + response->header.length, fsdetails->mnt_dir, 2);
+			response->body[response->header.length + 2] = '\0';
+			response->header.length += 3;
+		}
+		// ignore the final null
+		if (response->header.length) response->header.length--;
+	} else {
+		char path[request->header.length + 1];
+		srfp_to_dos_path(request->body, request->header.length, path);
+		response->body = malloc(65535);
+		response->header.length = 0;
+		DIR *dir = opendir(path);
+		if (dir){
+			struct dirent *entry;
+			uint8_t i;
+			while (entry = readdir(dir)){
+				for (i = 0;;i++){
+					response->body[response->header.length] = entry->d_name[i];
+					response->header.length++;
+					if (entry->d_name[i] == '\0') break;
+				}
+			}
+			// ignore the final null
+			if (response->header.length) response->header.length--;
+			closedir(dir);
+		} else {
+			printf("Couldn't open directory %s", path);
+			perror("");
+		}
+	}
+}
 
 void do_version(srfp_message *request, srfp_message *response){
 	response->body = malloc(3);
@@ -36,6 +79,7 @@ int main(){
 
 		switch (request.header.type){
 			case 0x01: //DirectoryList
+				do_directory_list(&request, &response);
 				break;
 			case 0x02: //NodeInfo
 				break;
--- util.c
+++ util.c
@@ -1,17 +1,19 @@
-void srfp_to_dos_path(char *str, size_t len){
-	char *strchr;
-	for (strchr = str; strchr < str + len; strchr++){
-		if (*strchr == '\0'){
-			*strchr = '\\';
-		}
-	}
-}
+/**
+Creates a **new** string with a DOS path from a buffer containing a SRFP path.
 
-void dos_to_srfp_path(char *str, size_t len){
-	char *strchr;
-	for (strchr = str; strchr < str + len; strchr++){
-		if (*strchr == '\\'){
-			*strchr = '\0';
+Argument 'out' is the string to copy into, which should be initialised with a
+length of len + 1.
+*/
+void srfp_to_dos_path(char *str, size_t len, char *out){
+	// we can't do this in place b/c the equivalent C string is 1 byte longer
+	// due to the trailing null, so we create a new string and return that
+	size_t i;
+	for (i = 0; i < len; i++){
+		if (str[i] == '\0'){
+			out[i] = '\\';
+		} else {
+			out[i] = str[i];
 		}
 	}
+	out[len] = '\0';
 }
